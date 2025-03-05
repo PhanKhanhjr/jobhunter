0a3e5e4262cf70858675c58c96f770ecb94e5625
phankhanhjr <khanh0396289813@gmail.com>
2025-03-05 07:10:52 +0700

    init project

---
+++ src/test/java/jobhunter/JobhunterApplicationTests.java
@@ -0,0 +1,12 @@
+package jobhunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobhunterApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
