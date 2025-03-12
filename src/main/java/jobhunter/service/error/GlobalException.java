3f893d5b7cb6aabca0026d0e03b0000ea2fe546a
phankhanhjr <khanh0396289813@gmail.com>
2025-03-12 23:08:40 +0700

    feat(api): format exception response

--- src/main/java/jobhunter/service/error/GlobalException.java
+++ src/main/java/jobhunter/service/error/GlobalException.java
@@ -1,5 +1,7 @@
 package jobhunter.service.error;
 
+import jobhunter.domain.RestResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,7 +9,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalException {
     @ExceptionHandler(value = IdInvalidException.class)
-    public ResponseEntity<String> handleIdException(IdInvalidException idInvalidException){
-        return ResponseEntity.badRequest().body(idInvalidException.getMessage());
+    public ResponseEntity<RestResponse<Object>> handleIdException(IdInvalidException idInvalidException) {
+        RestResponse<Object> restResponse = new RestResponse<Object>();
+        restResponse.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        restResponse.setError(idInvalidException.getMessage());
+        restResponse.setMessage("IdInvalidException");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(restResponse);
     }
 }
